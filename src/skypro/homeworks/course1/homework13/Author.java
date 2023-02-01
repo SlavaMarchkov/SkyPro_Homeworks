5466ae2ccb396df2a44d8cef38b9920a243825f6
SlavaMarchkov <slavamarchkov@yandex.ru>
2023-02-02 00:53:17 +0700

    Добавляет класс Author: конструктор, геттеры

---
+++ src/skypro/homeworks/course1/homework13/Author.java
@@ -0,0 +1,21 @@
+package skypro.homeworks.course1.homework13;
+
+public class Author {
+
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+}
