2181ad7e890b26bc62199a3d664b2f7a18e1bd57
SlavaMarchkov <slavamarchkov@yandex.ru>
2023-03-22 17:36:04 +0700

    Реализует домашнее задание урока 2.4 Работа с исключениями

---
+++ src/skypro/homeworks/course2/homework04/WrongLoginException.java
@@ -0,0 +1,7 @@
+package skypro.homeworks.course2.homework04;
+
+public class WrongLoginException extends Exception {
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
