4cc0ad993209702a5e98c83839caf0a2fcffe0c4
SlavaMarchkov <slavamarchkov@yandex.ru>
2023-04-03 22:22:24 +0700

    Доработки после просмотра примера выполнения ДЗ

--- src/skypro/homeworks/course2/homework06/Main.java
+++ src/skypro/homeworks/course2/homework06/Main.java
@@ -3,6 +3,11 @@ package skypro.homeworks.course2.homework06;
 import java.util.*;
 
 public class Main {
+
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7, 13, 26, 31, 1_000, 1_003));
+    private static final List<Integer> numsList = new ArrayList<>(List.of(48, 16, 16, 24, 1, 0, 1, 8, 2, 3, 5, 5, 6, 7, 2, 10, 4, 4, 8, 64, 32, 9));
+    private static final List<String> words = List.of("cat", "food", "dog", "cat", "eat", "Bob", "Bill", "Bob", "dog");
+
     public static void main(String[] args) {
         task1();
         task2();
@@ -14,7 +19,6 @@ public class Main {
      * печатает только нечетные числа в консоль
      */
     private static void task1() {
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7, 13, 26, 31, 1_000, 1_003));
         for (int num : nums) {
             if (num % 2 != 0) {
                 System.out.print(num + " ");
@@ -27,34 +31,13 @@ public class Main {
      * печатает только четные числа без повторений в порядке возрастания
      */
     private static void task2() {
-        List<Integer> nums = new ArrayList<>(List.of(48, 16, 16, 24, 1, 0, 1, 8, 2, 3, 5, 5, 6, 7, 2, 10, 4, 4, 8, 64, 32, 9));
-        List<Integer> evenNums = new ArrayList<>();
-        for (int num : nums) {
-            if (num % 2 == 0) {
-                evenNums.add(num);
+        Collections.sort(numsList);
+        int prevNum = Integer.MIN_VALUE;
+        for (int num : numsList) {
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.print(num + " ");
+                prevNum = num;
             }
-        }
-
-        /*
-        // Вариант с циклом по ArrayList
-        Collections.sort(evenNums);
-
-        for (int i = 0; i < evenNums.size() - 1; i++) {
-            if (evenNums.get(i).equals(evenNums.get(i + 1))) {
-                evenNums.remove(evenNums.get(i + 1));
-            }
-            System.out.print(evenNums.get(i) + " ");
-        }
-        System.out.print(evenNums.get(evenNums.size() - 1));*/
-
-        // Вариант с Set
-        Set<Integer> uniqueEvenNums = new HashSet<>(evenNums);
-
-        List<Integer> sortedUniqueEvenNums = new ArrayList<>(uniqueEvenNums);
-        Collections.sort(sortedUniqueEvenNums);
-
-        for (Integer num : sortedUniqueEvenNums) {
-            System.out.print(num + " ");
         }
 
         System.out.println();
@@ -64,7 +47,6 @@ public class Main {
      * выводит в консоль все уникальные слова из списка слов, в котором могут встречаться дубли
      */
     private static void task3() {
-        List<String> words = List.of("cat", "food", "dog", "cat", "eat", "Bob", "Bill", "Bob", "dog");
         Set<String> uniqueWords = new HashSet<>(words);
 
         for (String word : uniqueWords) {
