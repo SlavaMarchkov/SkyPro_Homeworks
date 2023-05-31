23689e25607ed1d864464a714ddf93d00b0e020b
SlavaMarchkov <slavamarchkov@yandex.ru>
2023-05-31 19:48:09 +0700

    Доработки после разбора домашки по видеоуроку

--- src/skypro/homeworks/course2/homework15/IntegerListImpl.java
+++ src/skypro/homeworks/course2/homework15/IntegerListImpl.java
@@ -22,7 +22,7 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Integer add(final Integer item) {
-        validateSize();
+        growIfNeeded();
         validateItem(item);
         storage[size++] = item;
         return item;
@@ -30,7 +30,7 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Integer add(final int index, final Integer item) {
-        validateSize();
+        growIfNeeded();
         validateItem(item);
         validateIndex(index);
         if (index == size) {
@@ -76,7 +76,7 @@ public class IntegerListImpl implements IntegerList {
     @Override
     public boolean contains(final Integer item) {
         Integer[] storageCopy = toArray();
-        mergeSort(storageCopy);
+        sort(storageCopy);
         return binarySearch(storageCopy, item);
     }
 
@@ -148,7 +148,7 @@ public class IntegerListImpl implements IntegerList {
         }
     }
 
-    private void validateSize() {
+    private void growIfNeeded() {
         if (size == storage.length) {
             grow();
         }
@@ -160,59 +160,38 @@ public class IntegerListImpl implements IntegerList {
         }
     }
 
-    private void insertionSorting(Integer[] arr) {
-        int in, out;
-        for (out = 1; out < arr.length; out++) {
-            Integer temp = arr[out];
-            in = out;
-            while (in > 0 && arr[in - 1] >= temp) {
-                arr[in] = arr[in - 1];
-                in--;
-            }
-            arr[in] = temp;
+    private void sort(Integer[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(Integer[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
         }
     }
 
-    public static void mergeSort(Integer[] arr) {
-        if (arr.length < 2) {
-            return;
-        }
-        int mid = arr.length / 2;
-        Integer[] left = new Integer[mid];
-        Integer[] right = new Integer[arr.length - mid];
+    private static int partition(Integer[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
 
-        for (int i = 0; i < left.length; i++) {
-            left[i] = arr[i];
-        }
-
-        for (int i = 0; i < right.length; i++) {
-            right[i] = arr[mid + i];
-        }
-
-        mergeSort(left);
-        mergeSort(right);
-
-        merge(arr, left, right);
-    }
-
-    public static void merge(Integer[] arr, Integer[] left, Integer[] right) {
-
-        int mainP = 0;
-        int leftP = 0;
-        int rightP = 0;
-        while (leftP < left.length && rightP < right.length) {
-            if (left[leftP] <= right[rightP]) {
-                arr[mainP++] = left[leftP++];
-            } else {
-                arr[mainP++] = right[rightP++];
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swapElements(arr, i, j);
             }
         }
-        while (leftP < left.length) {
-            arr[mainP++] = left[leftP++];
-        }
-        while (rightP < right.length) {
-            arr[mainP++] = right[rightP++];
-        }
+
+        swapElements(arr, i + 1, end);
+        return i + 1;
+    }
+
+    private static void swapElements(Integer[] arr, int i1, int i2) {
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
     }
 
     private boolean binarySearch(Integer[] arr, Integer item) {
@@ -234,9 +213,7 @@ public class IntegerListImpl implements IntegerList {
     }
 
     private void grow() {
-        Integer[] newArray = new Integer[(int) (size * 1.5)];
-        System.arraycopy(storage, 0, newArray, 0, storage.length);
-        storage = newArray;
+        storage = Arrays.copyOf(storage, size + size / 2);
     }
 
 }
